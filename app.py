c95a3109034e1ea2f69d69ac0b7049d2c9d37681
ahkylez <ahkylez@gmail.com>
2025-09-28 01:21:44 -0700

    Added backtesting

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from scr.financial_math import *
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from scipy.optimize import minimize
+
 import numpy as np
 
 st.set_page_config(page_title="Simple Portfolio Optimizer")
@@ -38,7 +38,11 @@ with st.sidebar:
 
     # Slider for number of portfolios
     number_of_portfolios = st.slider(
-        "Number of portfolios to simulate", 100, 5000, 2500)
+        "Number of portfolios to simulate", 100, 100000, 2500)
+    
+    st.divider()
+    allow_short_selling_mvp = st.checkbox("Allow Short Selling for MVP", value=False)
+    
 
 # So no errors appear when no data is entered
 if not symbols:
@@ -47,9 +51,41 @@ if not symbols:
 
 # Do math
 po = PortfolioOptimizer(symbols)
-w_mvp = po.calculate_mvp()
-w_max_sharpe = po.compute_sharpe_ratio_weights(number_of_portfolios)
+w_mvp = po.calculate_optimized_mvp(allow_short_selling_mvp)
+w_max_sharpe, expectedReturn, expectedVolatility, sharpeRatio, maxIndex = po.compute_sharpe_ratio_weights(number_of_portfolios)
+returns_ef, volatility_opt = po.calulate_efficient_frontier(expectedReturn)
 
+# Metrics
+st.subheader("Portfolio Metrics")
+
+col1, col2 = st.columns(2)
+mvp_annual_return = po.annualized_return(w_mvp)
+mvp_annual_risk = po.portfolio_risk_annualized(w_mvp)
+
+max_sharpe_annual_return = po.annualized_return(w_max_sharpe)
+max_sharpe_annual_risk = po.portfolio_risk_annualized(w_max_sharpe)
+
+with col1:
+    st.markdown("Minimum Volatility Portolio")
+    st.metric(label="Annualized Return", value=f"{mvp_annual_return:.2%}")
+    st.metric(label="Annualized Risk", value=f"{mvp_annual_risk:.2%}")
+
+with col2: 
+    st.markdown("Max Sharpe Portfolio")
+    st.metric(label="Annualized Return", value=f"{max_sharpe_annual_return:.2%}")
+    st.metric(label="Annualized Risk", value=f"{max_sharpe_annual_risk:.2%}")
+
+# Weights to purchase
+
+st.subheader("Optimal Portfolio Weights")
+
+weights_df = pd.DataFrame({
+    "MVP Weight": w_mvp,
+    "Max Sharpe Weight": w_max_sharpe
+}, index=symbols) # Use the stock symbols as the index
+
+weights_df_formatted = weights_df.style.format("{:.2%}")
+st.dataframe(weights_df_formatted, use_container_width=True)
 
 
 # Plots
@@ -68,6 +104,51 @@ ax.set_title("Portfolio Weights: MVP vs Max Sharpe")
 ax.legend()
 st.pyplot(fig)
 
+## Main graph
+
+plt.figure(figsize=(12,10))
+plt.scatter(expectedVolatility, expectedReturn, c=sharpeRatio, cmap='viridis', label='Simulated Portfolios')
+plt.xlabel("Expected Volatility (Standard Deviation)")
+plt.ylabel("Expected Log Returns")
+plt.colorbar(label='Sharpe Ratio')
+plt.title("Portfolio Optimization with Efficient Frontier")
+
+Sigma_np = po.sigma.values if hasattr(po.sigma, "values") else po.sigma
+mvp_vol = float(np.sqrt(w_mvp @ Sigma_np @ w_mvp))
+mvp_ret = float(po.expected_log_returns @ w_mvp)
+
+# Plot Max Sharpe Point
+plt.scatter(expectedVolatility[maxIndex],expectedReturn[maxIndex], c='red', marker='*', s=300, edgecolors='k', linewidths=1.2, label='Max Sharpe Point')
+
+# Plot MVP
+plt.scatter(mvp_vol, mvp_ret, c='green', marker='X', s=200, edgecolors='k', linewidths=1.2, label='Minimum Volatility Portfolio (MVP)')
+
+# Plot the Efficient Frontier
+# Filter out NaNs if any optimization failed
+valid_ef_indices = ~np.isnan(volatility_opt)
+plt.plot(volatility_opt[valid_ef_indices], returns_ef[valid_ef_indices], linestyle='--', color='black', linewidth=2, label='Efficient Frontier')
+
+plt.legend()
+plt.grid(True, linestyle=':', alpha=0.7)
+st.pyplot(plt)
 
 
 
+results = po.out_of_sample_backtest()
+
+if results: # Check if the backtest was successful
+    plt.figure(figsize=(12, 6))
+    
+    # Plot the MVP portfolio value
+    results['MVP_Value'].plot(label='Minimum Volatility Portfolio (MVP)', linewidth=2)
+    
+    # Plot the Max Sharpe portfolio value
+    results['MaxSharpe_Value'].plot(label='Maximum Sharpe Ratio Portfolio', linewidth=2)
+    
+    plt.title('Out-of-Sample Portfolio Backtest (Starting Value: $10,000) 📈', fontsize=16)
+    plt.xlabel('Date', fontsize=12)
+    plt.ylabel('Portfolio Value ($)', fontsize=12)
+    plt.legend(loc='best')
+    plt.grid(True, linestyle='--', alpha=0.6)
+    plt.tight_layout()
+    st.pyplot(plt)
--- scr/financial_math.py
+++ scr/financial_math.py
@@ -2,24 +2,33 @@ import pandas as pd
 import numpy as np
 from numpy.linalg import inv
 import yfinance as yf
-
+from scipy.optimize import minimize
 
 class PortfolioOptimizer:
-    def __init__(self, symbols: list[str], trading_periods=252):
+    def __init__(self, symbols: list[str], trading_periods=252, prices: pd.DataFrame | None = None):
         self.symbols = symbols
         self.trading_periods = trading_periods
 
         self.prices = self._fetch_prices()
+        self.returns, self.log_returns = self._calculate_returns()
 
-        self.returns, self.log_returns = self._calculate_all_returns()
-
-
+        self.prices = prices if prices is not None else self._fetch_prices()
 
         self.expected_returns = self.returns.mean().to_numpy()
         self.expected_log_returns = self.log_returns.mean().to_numpy()
 
         self.n = len(self.symbols)
         self.cov_return_matrix = self._get_cov_matrix(self.returns)
+        
+        self.sigma = self.log_returns.cov()
+
+    @classmethod
+    def from_prices(cls, prices: pd.DataFrame, trading_periods=252):
+        # Infer symbols from columns
+        symbols = list(prices.columns)
+
+        # Call the normal constructor, but inject these symbols and this price data
+        return cls(symbols=symbols, trading_periods=trading_periods, prices=prices)
         
     def _fetch_prices(self, period='1y', interval="1d") -> pd.DataFrame:
         """
@@ -36,15 +45,11 @@ class PortfolioOptimizer:
         """
         if not self.symbols:
             return pd.DataFrame() 
-        
         try:
-
             stock_data = yf.download(tickers=self.symbols, period=period, interval=interval, progress=False)
-            
             if stock_data.empty:
                 print("No data found for given symbols")
                 return pd.DataFrame()
-            
             stock_df = pd.DataFrame(stock_data)
             price_df = stock_df["Close"].dropna()
             return price_df
@@ -52,7 +57,7 @@ class PortfolioOptimizer:
             return pd.DataFrame()
 
 
-    def _calculate_all_returns(self) -> tuple[pd.DataFrame, pd.DataFrame]:
+    def _calculate_returns(self) -> tuple[pd.DataFrame, pd.DataFrame]:
         simple_returns = self.prices.pct_change().dropna()
         log_returns = np.log(self.prices).diff().dropna()
         
@@ -67,6 +72,77 @@ class PortfolioOptimizer:
     # Get covarience of matrix
     def _get_cov_matrix(self, returns) -> np.ndarray:
         return returns.cov().to_numpy()
+    
+    # Efficient Frontier Calculations
+    def negativeSR(self, w):
+        w = np.array(w)
+        # Ensure expected_log_returns is a 1D array or Series
+        R = np.sum(self.expected_log_returns.values * w) if hasattr(self.expected_log_returns, 'values') else np.sum(self.expected_log_returns * w)
+        Sigma_np = self.sigma.values if hasattr(self.sigma, "values") else self.sigma
+        V = np.sqrt(np.dot(w.T, np.dot(Sigma_np,w)))
+        SR = R/V
+        return -1 * SR
+    
+    def checkSumToOne(self, w):
+        return np.sum(w) - 1
+
+    def minimizeMyVolatility(self, w):
+        w = np.array(w)
+        Sigma_np = self.sigma.values if hasattr(self.sigma, "values") else self.sigma
+        V = np.sqrt(np.dot(w.T, np.dot(Sigma_np,w)))
+        return V
+
+    def getReturn(self, w):
+        w = np.array(w)
+        # Ensure expected_log_returns is a 1D array or Series
+        R = np.sum(self.expected_log_returns.values * w) if hasattr(self.expected_log_returns, 'values') else np.sum(self.expected_log_returns * w)
+        return R
+    
+    # Calculate Efficient Frontier points
+    def calulate_efficient_frontier(self, expectedReturn):
+
+        w0 = np.full(self.n, 1.0/self.n)
+        bounds = ((0, 1),) * self.n
+        constraints = ({'type':'eq', 'fun':self.checkSumToOne})
+
+        # Determine min and max possible returns from simulated portfolios to set the range for the efficient frontier
+        min_return = expectedReturn.min()
+        max_return = expectedReturn.max()
+        returns_ef = np.linspace(min_return * 0.9, max_return * 1.1, 50) # Extend range slightly
+        volatility_opt = []
+
+        for R_target in returns_ef:
+            constraints = ({'type':'eq', 'fun':self.checkSumToOne},
+                        {'type':'eq', 'fun': lambda w: self.getReturn(w) - R_target})
+            opt = minimize(self.minimizeMyVolatility, w0, method='SLSQP', bounds=bounds, constraints=constraints)
+            if opt.success:
+                volatility_opt.append(opt['fun'])
+            else:
+                volatility_opt.append(np.nan) # Append NaN if optimization fails
+
+        # Convert to numpy array for plotting
+        volatility_opt = np.array(volatility_opt)
+        return returns_ef, volatility_opt
+    
+    def calculate_optimized_mvp(self, allow_short_selling: bool = False) -> np.ndarray:
+        w0 = np.full(self.n, 1.0 / self.n)
+        if allow_short_selling:
+            bounds = ((-1.0, 1.0),) * self.n
+        else:
+            bounds = ((0, 1),) * self.n
+
+        constraints = ({'type': 'eq', 'fun': self.checkSumToOne})
+        
+        opt = minimize(self.minimizeMyVolatility, w0, 
+                    method='SLSQP', 
+                    bounds=bounds, 
+                    constraints=constraints)
+        
+        if opt.success:
+            return opt.x
+        else:
+            print("MVP optimization failed. Returning equal weights.")
+            return w0
 
     # Get weights, explain math later in doc string
     def calculate_mvp(self) -> np.ndarray:
@@ -77,13 +153,14 @@ class PortfolioOptimizer:
         w_mvp = num/den
         return w_mvp
     
-    def compute_sharpe_ratio_weights(self, number_of_portfolios):
+    def compute_sharpe_ratio_weights(self, number_of_portfolios) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         weight = np.zeros((number_of_portfolios, self.n))
         expectedReturn = np.zeros(number_of_portfolios)
         expectedVolatility = np.zeros(number_of_portfolios)
         sharpeRatio = np.zeros(number_of_portfolios)
 
-        Sigma = self.log_returns.cov()
+        # Use self.sigma directly
+        Sigma = self.sigma
 
         for k in range(number_of_portfolios):
             # generate random weight vector
@@ -91,23 +168,18 @@ class PortfolioOptimizer:
             w = w / np.sum(w)
             weight[k,:] = w
             #expected log returns
-            expectedReturn[k] = np.sum(self.expected_log_returns * w)
+            # Use expected_log_returns directly
+            expectedReturn[k] = np.sum(self.expected_log_returns * w) 
             #expected volitility
             expectedVolatility[k] = np.sqrt(w.T @ Sigma @ w)
             # Sharpe Ratio
             sharpeRatio[k] = expectedReturn[k]/expectedVolatility[k]
 
         maxIndex = sharpeRatio.argmax()
-        max_sharpe_w = weight[maxIndex, :]
-        print(f"maxIndex, {maxIndex}/n")
-        print(f"max_sharpe_w[0], {max_sharpe_w}")
-        return max_sharpe_w
-
-
-
-
-
-
+        w_max_sharpe = weight[maxIndex, :]
+        # Return all the simulated data and the Max Sharpe weights
+        return w_max_sharpe, expectedReturn, expectedVolatility, sharpeRatio, maxIndex
+    
     # ---- Get portfolio metrics ----
     def portfolio_return(self, weights: np.ndarray):
         return weights @ self.expected_returns.T
@@ -129,9 +201,58 @@ class PortfolioOptimizer:
         annualized_std_dev = portfolio_std_dev_daily * np.sqrt(self.trading_periods)
         return annualized_std_dev
 
+        # Backtest data
+    def _simulate_portfolio_value(self, weights: np.ndarray, testing_prices: pd.DataFrame, initial_investment: float = 1.0) -> pd.Series:
+       
+        # Calculate daily simple returns for the testing period
+        testing_returns = testing_prices.pct_change().dropna()
+        
+        # Calculate the daily portfolio return
+        daily_portfolio_returns = pd.Series(
+            testing_returns.values @ weights,
+            index=testing_returns.index
+        )
+        
+        # Calculate the cumulative returns
+        cumulative_returns = (1 + daily_portfolio_returns).cumprod()
+        
+        # Calculate the portfolio value
+        portfolio_value = initial_investment * cumulative_returns
+        
+        return portfolio_value
 
 
+    def out_of_sample_backtest(self, training_days: int = 60, initial_investment: float = 1.0) -> dict[str, pd.Series]:
 
+        df_prices = self._fetch_prices(period='2y') # Get 2 years of data
+        if df_prices.empty:
+            print("Empty DF: Could not fetch enough data for backtest.")
+            return {}
+        
+        if len(df_prices) < training_days * 1.5:
+            print(f"Not enough data for backtest. Need at least {training_days} training days and some testing days.")
+            return {}
 
+        # Split Data
+        training_prices = df_prices.iloc[:training_days]
+        testing_prices = df_prices.iloc[training_days:]
+        
+        # Train the Optimizer on the training data
+        po_train = PortfolioOptimizer.from_prices(training_prices, trading_periods=self.trading_periods)
 
+        # Calculate Weights
+        w_mvp_opt = po_train.calculate_optimized_mvp(allow_short_selling=False)
+        w_sharpe, _, _, _, _ = po_train.compute_sharpe_ratio_weights(number_of_portfolios=5000)
 
+        results = {}
+        
+        # MVP Simulation
+        mvp_value_series = self._simulate_portfolio_value(w_mvp_opt, testing_prices, initial_investment)
+        results['MVP_Value'] = mvp_value_series
+        
+        # Max Sharpe Simulation
+        sharpe_value_series = self._simulate_portfolio_value(w_sharpe, testing_prices, initial_investment)
+        results['MaxSharpe_Value'] = sharpe_value_series
+        
+
+        return results
